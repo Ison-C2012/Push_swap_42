ecf76f6b4d68f640f7245673cba2093f2b2daf7a
Ison Kislovodsk <ison.c.2012.tk@gmail.com>
2025-11-22 21:34:05 +0900

    edit main

--- proj/srcs/main/main.c
+++ proj/srcs/main/main.c
@@ -6,12 +6,13 @@
 /*   By: keitotak <keitotak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 17:13:55 by keitotak          #+#    #+#             */
-/*   Updated: 2025/11/22 19:27:57 by keitotak         ###   ########.fr       */
+/*   Updated: 2025/11/22 21:02:54y keitotak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+#define SUCCESS 0
 #define NO_ARG 1
 
 /*main func checks its arguments and pass them to push_swap func.*/
@@ -23,7 +24,7 @@ int	main(int argc, char *argv[])
 	t_stack	*b;
 
 	if (argc == NO_ARG)
-		return (0);
+		return (SUCCESS);
 	array = get_arg_into_arr(&argv[1], argc - 1);
 	a = stack_create(array, argc - 1);
 	free(array);
@@ -31,5 +32,5 @@ int	main(int argc, char *argv[])
 	push_swap(a, b);
 	stack_free(a);
 	stack_free(b);
-	return (0);
+	return (SUCCESS);
 }
